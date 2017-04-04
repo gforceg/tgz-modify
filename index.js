7153d105329dcc715243c2f3d73d5965f99f5a2b
Hedin <greg.hedin@ssa.gov>
2017-04-04 13:08:32 -0600

    pretty comments

--- index.js
+++ index.js
@@ -20,43 +20,34 @@ input.pipe(gunzip) // unzip the tar
 .pipe(extract) // parse the tar
   .on('entry', (header, stream, next) => {
     let file = header.name
-    // read package/package.json
-    if (file == 'package/package.json') {
-      console.log(chalk.yellow('found package/package.json'))
+    if (file == 'package/package.json') { // read package/package.json
       let package_json_contents = '';
-      // fill the string buffer
-      stream.on('data', (d) => {
+
+      stream.on('data', (d) => { // fill the string buffer
         package_json_contents += new Buffer(d, 'utf8').toString()
       })
-      // inflate and modify the json
-      stream.on('end', () => {
+
+      stream.on('end', () => { // inflate and modify the json
         let package_json_obj = JSON.parse(package_json_contents)
         package_json_obj.name = 'not-ts-clipboard-anymore'
         package_json_obj.devDependencies['omg'] ='1.0.0'
         let package_json_buffer = JSON.stringify(package_json_obj, null, '\t')
-
-        stream.pipe(pack.entry(header, package_json_buffer, next))
+        
+        stream.pipe(pack.entry(header, package_json_buffer, next))  //replace the existing package_json_buffer w/ the new one
         stream.end()
       })
     } else {
-      let data = ''
-        // if it isn't package/package.json, then just repipe the original data
+      let data = '' // if it isn't package/package.json, then just repipe the original data
       stream.on('data', (d) => data += new Buffer(d, 'utf8').toString())
-      stream.on('end', () => {
-        console.log('repiping original', header.name)
-        stream.pipe(pack.entry(header, data, next))
-        next()
-      })
+      stream.on('end', () => { stream.pipe(pack.entry(header, data, next)) ; next() })
     }
   })
   .on('end', () => {
     console.log('EOF')
   })
   .on('finish', () => {
-    pack.finalize()
+    pack.finalize() // >> .tar
   })
 
-  pack.pipe(gzip)
-  .pipe(outstream)
-  
-// })
+  pack.pipe(gzip) // >> .tgz
+  .pipe(outstream) // >> outfile.tgz
