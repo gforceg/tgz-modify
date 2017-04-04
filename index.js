4ca748fc675942f67721e961c98c3e12bad77db5
Hedin <greg.hedin@ssa.gov>
2017-04-04 09:32:01 -0600

    bring in chalk

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-let zlib = require('zlib')
-let path = require('path')
-let fs = require('fs')
-let stream = require('stream')
+const zlib = require('zlib')
+const path = require('path')
+const fs = require('fs')
+const stream = require('stream')
+const chalk = require('chalk')
 
-let tgz = fs.readFileSync('./package.tgz')
-console.dir(tgz)
-let tar_stream = new stream.Stream()
-tar_stream.pipe(tgz)
-tar_stream.on('data', (data) => {
-  console.dir(data)
+// let tgz = fs.readFileSync('./package.tgz')
+// console.dir(tgz)
+// let tar_stream = new stream.Stream()
+// tar_stream.pipe(tgz)
+
+// tar_stream.on('data', (data) => {
+  // console.dir(data)
+// })
+
+
+const r = fs.createReadStream('test.txt', {encoding: 'utf8'})
+
+r.on('readable', () => {
+  console.log(chalk.blue('readable'))
+  console.log(r.read())
+})
+
+r.on('end', () => {
+  console.log(chalk.blue('end'))
+  r.close()
+})
+
+const w = fs.createWriteStream('test.txt', {flags: 'r+', defaultEncoding: 'utf8', fd: null, mode: 0o666, autoClose: true})
+
+w.on('open', () => {
 })
