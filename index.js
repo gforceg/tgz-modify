81da52e3c1e5d3cfb43941a8c0c5a31c7e2f0590
Hedin <greg.hedin@ssa.gov>
2017-04-04 11:58:45 -0600

    extract package.json and write to files/test.json

--- index.js
+++ index.js
@@ -3,30 +3,44 @@ const path = require('path')
 const fs = require('fs')
 const stream = require('stream')
 const chalk = require('chalk')
+const child_process = require('child_process')
+const tar = require('tar')
+// let tgz_file = path.join('files', 'package.tgz')
+// let tar_file = path.join('files', 'package.tar')
 
-// let tgz = fs.readFileSync('./package.tgz')
-// console.dir(tgz)
-// let tar_stream = new stream.Stream()
-// tar_stream.pipe(tgz)
+let tgz_file = 'files/package.tgz'
+let tar_file = 'files/package.tar'
+const input = fs.createReadStream(tgz_file)
+// const output = fs.createWriteStream(tar_file)
 
-// tar_stream.on('data', (data) => {
-  // console.dir(data)
+const gunzip = zlib.createGunzip()
+const gzip = zlib.createGzip()
+
+input.pipe(gunzip) // unzip the tar
+.pipe(tar.Parse()) // parse the tar
+  .on('entry', (e) => {
+    let file = e.path
+    // read package/package.json
+    if (file == 'package/package.json') {
+      console.log(chalk.yellow('found package/package.json'))
+      let package_json_contents = '';
+      // fill the string buffer
+      e.on('data', (d) => {
+        package_json_contents += new Buffer(d, 'utf8').toString()
+      })
+      // inflate and modify the json
+      e.on('end', () => {
+        let package_json_obj = JSON.parse(package_json_contents)
+        package_json_obj.name = 'not-ts-clipboard-anymore'
+        package_json_obj.devDependencies['omg'] ='1.0.0'
+        new fs.createWriteStream('files/test.json').write(JSON.stringify(package_json_obj, null, '\t'))
+        // e.pipe(tar.Pack({ name: 'package/package.json'}, JSON.stringify(package_json_obj)))
+        // e.pipe(new fs.createWriteStream('files/test.json'))
+        // .pipe(gzip)
+      })
+    }
+  })
+  .on('end', () => {
+    console.log('EOF')
+  })
 // })
-
-
-const r = fs.createReadStream('test.txt', {encoding: 'utf8'})
-
-r.on('readable', () => {
-  console.log(chalk.blue('readable'))
-  console.log(r.read())
-})
-
-r.on('end', () => {
-  console.log(chalk.blue('end'))
-  r.close()
-})
-
-const w = fs.createWriteStream('test.txt', {flags: 'r+', defaultEncoding: 'utf8', fd: null, mode: 0o666, autoClose: true})
-
-w.on('open', () => {
-})
